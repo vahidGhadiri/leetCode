81aef4885031cf90c1957b726e3d3b7c71389bb4
Vahid Ghadiri <va.ghadiri@roundtableapps.com>
2024-06-18 10:18:04 +0300

    contains duplications

---
+++ containsDuplicate.ts
@@ -0,0 +1,12 @@
+function containsDuplicate(nums: number[]): boolean {
+    const set = new Set<number>();
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        if (set.has(num)) {
+            return true; 
+        }
+        set.add(num);
+    }
+    return false; 
+}
+
