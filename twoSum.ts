7e4c269ea082e32c3df5add33fd788ae37fab5cc
Vahid Ghadiri <whydrf@gmail.com>
2024-06-24 09:59:27 +0300

    palindrome-numbers

--- twoSum.ts
+++ twoSum.ts
@@ -1,3 +1,12 @@
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+ * @param numbers 
+ * @param target 
+ * @returns 
+ */
+
 function twoSum(numbers: number[], target: number): number[] {
     const hashMap = new Map()
     for (let i = 0; i < numbers.length; i++) {
