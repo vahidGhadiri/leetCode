7ad28c264828e84d34d138dda0aa50408f274500
Vahid Ghadiri <whydrf@gmail.com>
2024-07-03 12:20:55 +0300

    insertion sort

---
+++ insertionSort.ts
@@ -0,0 +1,14 @@
+const insertionSort = (arr: number[]): number[] => {
+    for (let i = 0; i < arr.length; i++) {
+        let current = arr[i]
+        let j = i - 1
+        while (j >= 0 && arr[j] > current) {
+            arr[j + 1] = arr[j]
+            j--
+        }
+        arr[j + 1] = current
+    }
+
+
+    return arr
+}
