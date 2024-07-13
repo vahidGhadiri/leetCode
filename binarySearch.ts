33ad55ae1c92474def06fd910d3e0703a9136925
Vahid Ghadiri <whydrf@gmail.com>
2024-07-13 09:21:57 +0300

    Count all the nodes in binary tree

--- binarySearch.ts
+++ binarySearch.ts
@@ -4,7 +4,7 @@ You must write an algorithm with O(log n) runtime complexity.
 
 Example 1:
 Input: nums = [-1,0,3,5,9,12], target = 9
-Output: 4
+Output: 4 
 Explanation: 9 exists in nums and its index is 4
 
 Example 2:
