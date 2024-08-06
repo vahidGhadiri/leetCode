8e2a58b46785bf40aa480c2fb3393b6fe5f72b2c
Vahid Ghadiri <whydrf@gmail.com>
2024-08-06 08:37:42 +0300

    (data-structures/search): Add linear search

---
+++ algorithms/search/linearSearch.ts
@@ -0,0 +1,8 @@
+function linearSearch<T>(array: T[], target: T): number {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === target) {
+            return i;
+        }
+    }
+    return -1;
+}
