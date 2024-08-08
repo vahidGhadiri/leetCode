304978bcd7236b0809e2ce53ee9240de8ea78af4
vahidGhadiri <whydrf@gmail.com>
2024-08-08 18:53:24 +0330

    ALG(general): Add redcue method to array protoype

--- general/reduce.ts
+++ general/reduce.ts
@@ -10,4 +10,35 @@ const reduce = <T, U>(arr: T[], fn: (acc: U, curr: T, index: number, arr: T[]) =
 
 const numbers = [1, 2, 3, 4, 5];
 const sum = reduce(numbers, (acc, curr) => acc + curr, 0);
-console.log(sum); 
+console.log(sum);
+
+interface Array<T> {
+    myReduce<U>(
+        fn: (acc: U, curr: T, index: number, arr: T[]) => U,
+        initialValue?: U
+    ): U;
+}
+
+Array.prototype.myReduce = function <T, U>(
+    fn: (acc: U, curr: T, index: number, arr: T[]) => U,
+    initialValue?: U
+): U {
+    if (this.length === 0 && initialValue === undefined) throw new TypeError('Reduce of empty array with no initial value')
+    if (this.length === 0 && initialValue !== undefined) return initialValue;
+
+    let accumulator: U;
+
+    if (initialValue === undefined) {
+        accumulator = this[0] as unknown as U;
+        for (let i = 1; i < this.length; i++) {
+            accumulator = fn(accumulator, this[i], i, this);
+        }
+    } else {
+        accumulator = initialValue;
+        for (let i = 0; i < this.length; i++) {
+            accumulator = fn(accumulator, this[i], i, this);
+        }
+    }
+
+    return accumulator;
+};
