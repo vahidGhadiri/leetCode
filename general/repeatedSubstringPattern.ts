70a2ac0b43e4a888b4675e70f1b9a47ac9844f59
Vahid Ghadiri <whydrf@gmail.com>
2024-08-19 09:59:04 +0300

    ALG(general): LEET_CODE_459 Repeated substring pattern - (KMP algorithm)

--- general/repeatedSubstringPattern.ts
+++ general/repeatedSubstringPattern.ts
@@ -28,3 +28,37 @@ function repeatedSubstringPattern(s: string): boolean {
     const ss = (s + s).slice(1, -1);
     return ss.includes(s);
 };
+
+
+/**
+ * Solution #2: KMP
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ * @param s 
+ * @returns 
+ */
+function repeatedSubstringPattern2(s: string): boolean {
+    const n = s.length;
+    const lps: number[] = new Array(n).fill(0);
+
+    let length = 0; 
+    let i = 1;
+
+    while (i < n) {
+        if (s[i] === s[length]) {
+            length++;
+            lps[i] = length;
+            i++;
+        } else {
+            if (length !== 0) {
+                length = lps[length - 1];
+            } else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+    const len = lps[n - 1];
+    return len > 0 && n % (n - len) === 0;
+}
+
